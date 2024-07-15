0d16e45712431a208cda51b878a84abfc4ef912d
Maheshwarigadakh <maheshwarigadakh2003@gmail.com>
2024-07-15 11:51:41 +0530

    2 code add

--- index.js
+++ index.js
@@ -5,3 +5,12 @@ const students = [
 for(const stud of students){
     console.log(`Good Morning ${stud}`)
 }
+
+const marks =[55,88,66,98,69,87,94,56]
+let largest = marks[0]
+for(const num of marks){
+    if(num >largest){
+        largest = num
+    }
+}
+console.log(largest)
